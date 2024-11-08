6ef59311d730545bed1292f6d338c26c3505319a
Marcos Eduardo <marcosdudu001@gmail.com>
2024-11-08 12:11:05 -0300

    integration

--- src/database.js
+++ src/database.js
@@ -1,17 +1,20 @@
-const express = require("express")
-const app = express()
-const mysql = require("mysql")
-const bcrypt = require("bcrypt")
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const bcrypt = require("bcrypt");
+const cors = require("cors"); // Importando o pacote CORS
 
-require("dotenv").config()
+require("dotenv").config();
 const generateAccessToken = require("./generateAccessToken");
 
+// Habilitando CORS para todas as origens
+app.use(cors()); // Permite todas as origens (para desenvolvimento)
 
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_DATABASE = process.env.DB_DATABASE
-const DB_PORT = process.env.DB_PORT
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT;
 console.log(process.env.DB_DATABASE);
 
 const db = mysql.createPool({
@@ -21,9 +24,10 @@ const db = mysql.createPool({
   password: DB_PASSWORD,
   database: DB_DATABASE,
   port: DB_PORT
-})
+});
 
-app.use(express.json())
+app.use(express.json());
+
 app.post("/createUser", async (req, res) => {
   const { name, cpf, email, phone, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -42,7 +46,7 @@ app.post("/createUser", async (req, res) => {
       if (result.length != 0) {
         connection.release();
         console.log("----> User already exists");
-        res.sendStatus(409);
+        res.sendStatus(409); // Usuário já existe
       } else {
         const sqlInsert = "INSERT INTO lajeado_users (name, cpf, email, phone, password) VALUES (?, ?, ?, ?, ?)";
         const insert_query = mysql.format(sqlInsert, [name, cpf, email, phone, hashedPassword]);
@@ -52,7 +56,7 @@ app.post("/createUser", async (req, res) => {
           if (err) throw err;
           console.log("----> Created new User");
           console.log(result.insertId);
-          res.sendStatus(201);
+          res.sendStatus(201); // Status de sucesso ao criar o usuário
         });
       }
     });
@@ -62,40 +66,34 @@ app.post("/createUser", async (req, res) => {
 app.post("/login", (req, res) => {
   const { cpf, password } = req.body;
 
-  db.getConnection ( async (err, connection) => {
-    if (err) throw (err)
-    const sqlSearch = "SELECT * FROM lajeado_users WHERE cpf = ?"
-    const search_query = mysql.format(sqlSearch,[cpf])
+  db.getConnection(async (err, connection) => {
+    if (err) throw err;
+    const sqlSearch = "SELECT * FROM lajeado_users WHERE cpf = ?";
+    const search_query = mysql.format(sqlSearch, [cpf]);
 
-    await connection.query (search_query, async (err, result) => {
-      connection.release()
-      if (err) throw (err)
+    await connection.query(search_query, async (err, result) => {
+      connection.release();
+      if (err) throw err;
       if (result.length == 0) {
-        console.log("----> User does not exist")
-        res.sendStatus(404)
-      }
-      else {
-        const hashedPassword = result[0].password
+        console.log("----> User does not exist");
+        res.sendStatus(404);
+      } else {
+        const hashedPassword = result[0].password;
 
         if (await bcrypt.compare(password, hashedPassword)) {
-          console.log("----> Login Successful")
-          console.log("----> Generating accessToken")
-          const token = generateAccessToken({cpf: cpf})
-          console.log(token)
-          res.json({accessToken: token})
-        }
-        else {
-          res.send("Password incorrect!")
+          console.log("----> Login Successful");
+          console.log("----> Generating accessToken");
+          const token = generateAccessToken({ cpf: cpf });
+          console.log(token);
+          res.json({ accessToken: token });
+        } else {
+          res.send("Password incorrect!");
         }
       }
+    });
+  });
+});
 
-    })
-  })
-})
+const port = process.env.PORT;
 
-
-const port = process.env.PORT
-
-app.listen(port,
-  ()=> console.log(`Server Started on port ${port}...`)
-)
+app.listen(port, () => console.log(`Server Started on port ${port}...`));
