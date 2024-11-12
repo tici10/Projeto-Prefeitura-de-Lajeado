ae2108a7a3bfbb1df3786c55516cdd82f9987547
MR-MARCOSs <marcosdudu001@gmail.com>
2024-11-11 23:05:25 -0300

    search_data

---
+++ src/search_data.js
@@ -0,0 +1,62 @@
+let dados = [
+    {
+        "titulo": "Agendamento de Consultas - Unidade Básica de Saúde",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "agendamento consulta saúde unidade básica médico"
+    },
+    {
+        "titulo": "Solicitação de Medicamentos",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "medicamentos farmácia solicitação remédios"
+    },
+    {
+        "titulo": "Tratamento Odontológico",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "odontologia dentes tratamento dentista"
+    },
+    {
+        "titulo": "Atendimento Veterinário",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "veterinário animais saúde animal cuidados com pets"
+    },
+    {
+        "titulo": "Transporte de Paciente para Tratamento Médico",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "transporte paciente tratamento médico saúde"
+    },
+    {
+        "titulo": "Vigilância Sanitária",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "vigilância saúde pública sanitário fiscalização"
+    },
+    {
+        "titulo": "Vigilância Epidemológica",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "epidemiologia vigilância saúde pública doenças"
+    },
+    {
+        "titulo": "Fisioterapia",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "fisioterapia reabilitação tratamento saúde"
+    },
+    {
+        "titulo": "Vigilância em Saúde",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "vigilância saúde monitoramento prevenção"
+    },
+    {
+        "titulo": "Matrículas",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "matrícula educação escola matrícula escolar"
+    },
+    {
+        "titulo": "Transporte Escolar",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "transporte escolar estudantes educação"
+    },
+    {
+        "titulo": "Atendimento de Psicólogo e Assistente Escolar",
+        "link": "https://pt.wikipedia.org/wiki/meu_link",
+        "tags": "psicólogo assistente escolar atendimento educação apoio psicológico"
+    }
+];
