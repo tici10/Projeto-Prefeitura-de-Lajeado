557212520fca71552d3364db50f4debdb8c9c039
Ticiane <114961257+tici10@users.noreply.github.com>
2024-11-21 13:33:09 -0300

    Add files via upload

---
+++ js/script_button.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll('.solicitarButton');
+
+// Adicionamos um evento de clique a cada botão
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // Redireciona para outra página ou executa alguma lógica
+        window.location.href = './servico_page.html';
+    });
+});
